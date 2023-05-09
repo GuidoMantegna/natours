c475ddc3de3772d6483d66be1a6f5bfbf0bdec53
Guido Mantegna <gmantegna@elcallao.com>
2023-05-09 08:06:57 -0300

    Install mongoose and connect the app with the hosted DB

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-/** it's a good practice to have everything that is related to express in one file,
-and then everything that is related to the server in another main file. */
+// 1st we require our mongoose package
+const mongoose = require('mongoose');
 // Require dotenv to consume the config.env file
 const dotenv = require('dotenv');
 /* dotenv has a variable called config on it and then in there we just have
@@ -12,6 +12,26 @@ const app = require('./app');
 
 // This env is set by Express
 console.log(app.get('env'));
+
+// We replace the placeholder <password> with the real password
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+// 2nd we access to mongoose and call the connect method
+/* into this connect method, we need to pass in:
+1 - our database connection string. 
+2 - An object with some options that we need to specify in order to deal with some deprecations warnings */
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('DB connection successful!');
+  });
 
 // DEFINE THE PORT
 const port = process.env.PORT || 3000;
