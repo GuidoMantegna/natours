a07bd8f13856c6e5058ff151c24093f136c61d03
Guido Mantegna <gmantegna@elcallao.com>
2023-05-10 07:30:32 -0300

    Adds the first tour model with mongoose

--- server.js
+++ server.js
@@ -33,6 +33,27 @@ mongoose
     console.log('DB connection successful!');
   });
 
+// SCHEMA
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  }, // schema type options for each field
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// MODEL
+// NAME, SCHEMA - model name (tour) should be with an uppercase "T" as a convention
+const Tour = mongoose.model('Tour', tourSchema);
+
 // DEFINE THE PORT
 const port = process.env.PORT || 3000;
 
