6a5f0a54e42f8f5bacaab9f09ba7adecdd8e9612
Guido Mantegna <gmantegna@elcallao.com>
2023-05-16 08:55:46 -0300

    Creates a script to delete & import all the data from the collection

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    /* The create method can also accept an array of objects. In that case
+    it will then simply create a new document for each of the objects in the array. */
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    /* deleteMany() without params would then delete
+    all of the documents in a certain collection */
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+console.log(process.argv);
+
+/* [
+    'C:\\Program Files\\nodejs\\node.exe',
+    'C:\\Users\\Lenovo\\Documents\\Guido\\natours\\dev-data\\data\\import-dev-data.js',
+    '--import'
+   ]
+  1st arg is where the node command is located
+  2nd arg is the path to the file 
+  3rd arg will define the action itself */
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
