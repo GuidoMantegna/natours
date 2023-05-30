62bbb56d6baf7057a3ff2f62bdc8474528e0d8ef
Guido Mantegna <gmantegna@elcallao.com>
2023-05-30 07:53:41 -0300

    Creates a class which contains all getAllTours() methods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   // 1st we set the qty of results
@@ -8,66 +9,20 @@ exports.aliasTopTours = (req, res, next) => {
   // 3rd specify some fields so that the user doesn't get all the fields
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
-}
+};
 
 exports.getAllTours = async (req, res) => {
   try {
-    // FIRST WE BUILD THE QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    // here we replace (gte, gt, lte, lt) x ($gte, $gt, $lte, $lt)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // WE SAVE THE QUERY
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      /* .sort() is one of the many methods that are available on all documents created with query class. */
-      /* If a string is passed, it must be a space delimited list of path names so, 
-      instead of passing it into sort method as we received from the query { sort: '-price,ratingsAverage' },
-      we need to pass it this way '-price ratingsAverage' */
-
-      const sortBy = req.query.sort.split(',').join(' ');
-
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-
-      query = query.select(fields);
-    } else {
-      // minus '-' means to exclude something
-      query = query.select('-__V');
-    }
-
-    // 4) Pagination
-
-    //'*1' converts a string to number - '|| value' defines a default value
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    // e.g. (3 - 1) * 10 = 20 --> will skip 20 results, and return from 21 to 30 (page 3)
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     // WE EXECUTE THE QUERY
-    const tours = await query;
-    // query.sort().select().skip().limit()
+    /* here, we need to pass a query object (Tour.find()) and the queryString (req.query),
+    and then call the methods we want */
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
---
+++ utils/apiFeatures.js
@@ -0,0 +1,57 @@
+class APIFeatures {
+  // Here we have the mongoose query, and also the queryString that we get from Express
+  constructor(query, queryString) {
+    // query will come from Tour.find()
+    this.query = query;
+    // queryString will come from req.query
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    // Here we return the entire object so then we can chain all the methods
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__V');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
