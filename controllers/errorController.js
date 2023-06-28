1312d99fc5800d12a845acc989b9bb8022391451
Guido Mantegna <gmantegna@elcallao.com>
2023-06-28 17:49:13 -0300

    Difference errors for dev & prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,41 @@
-module.exports = (err, req, res, next) => {
-  // err.stack will basically show us where the error happened.
-  // console.log(err.stack)
-  // if err.statusCode is not defined we define it as 500 
-  err.statusCode = err.statusCode || 500;
-  // if err.status is not defined we define it as 'error' 
-  err.status = err.status || 'error';
-
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
-  })
-}
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+  // Programming ot other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  // if err.statusCode is not defined we define it as 500
+  err.statusCode = err.statusCode || 500;
+  // if err.status is not defined we define it as 'error'
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+};
