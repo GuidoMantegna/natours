29133db45b944ef48a71450ec043405f541c0340
Guido Mantegna <gmantegna@elcallao.com>
2023-06-28 17:01:50 -0300

    Add '404 not found' errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   // 1st we set the qty of results
@@ -36,6 +37,12 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id); // Tour.findOne({ _id: req.params.id })
+
+  if (!tour) {
+    /* we call next() with an error in order to jump straight into our error handling middleware. 
+    We use AppError classes defining the 'message' & 'statusCode' */
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -63,6 +70,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true, // this way, then the new updated document is the one that will be returned.
     runValidators: true, // each time that we update a certain document, then the validators that we specified in the schema will run again
   });
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -72,7 +84,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(204).json({
     status: 'success',
     data: null,
