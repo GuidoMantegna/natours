83f27394cf4eafbef213098566ed9c58a0cc501a
Guido Mantegna <gmantegna@elcallao.com>
2023-07-06 07:53:25 -0300

    Add passwordConfirm validation and password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,32 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // the callback() will be called when the new document is created
+      // THIS ONLY WORKS ON CREATE AND SAVE: whenever we want to update a user,
+      // we will always have to use SAVE as well and not, for example, 'find one and update'.
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  /* We only want to encrypt the password if the password field
+  has been updated, if it is changed or when it's created a new one */
+  if (!this.isModified('password')) return next();
+
+  /* The second param we need to specify is a cost parameter. We could actually do this in two ways:
+  1. manually generating the salt (the random string basically), then is gonna be added to our password.
+  2. we can also simply pass a cost parameter. That is basically a measure of how CPU intensive this 
+  operation will be */
+  // bcrypt.hash() will return a promise so the func() should be async
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined; // we don't need to store the passwordConfirm
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
