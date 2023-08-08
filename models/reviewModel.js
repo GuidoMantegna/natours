69425b4b80c189fb75cb9ce54a02c7f2789369ca
Guido Mantegna <gmantegna@elcallao.com>
2023-08-08 17:34:47 -0300

    Populate 'user' and 'tour' for reviewSchema

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,39 +1,50 @@
 const mongoose = require('mongoose');
-const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
     review: {
       type: String,
-      required: [true, 'Review can not be empty!']
+      required: [true, 'Review can not be empty!'],
     },
     rating: {
       type: Number,
       min: 1,
-      max: 5
+      max: 5,
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
     tour: {
       // here each Review document now knows what tour it belongs to.
       type: mongoose.Schema.ObjectId,
       ref: 'Tour',
-      required: [true, 'Review must belong to a tour.']
+      required: [true, 'Review must belong to a tour.'],
     },
     user: {
       // here each Review document now knows what user it belongs to.
       type: mongoose.Schema.ObjectId,
       ref: 'User',
-      required: [true, 'Review must belong to a user']
-    }
+      required: [true, 'Review must belong to a user'],
+    },
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
+
+/* Here we apply the populate to all the review queries which include 'find' */
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user', // prop to populate
+    select: 'name photo', // field to get
+  }).populate({
+    path: 'tour',
+    select: 'name' // field to get
+  });
+  next();
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
