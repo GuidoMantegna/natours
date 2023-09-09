62aa353dede0a79c64b6f11d235571e925237c90
Guido Mantegna <gmantegna@elcallao.com>
2023-09-09 11:09:13 -0300

    Set up leaflet for maps/locations

--- app.js
+++ app.js
@@ -17,15 +17,44 @@ const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARES
 // Serving static files
 /* here we pass the directory from which we want to serve static files. */
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security HTTP headers
 app.use(helmet());
+
+// LEAFLET CONFIG ----------------------------
+// Further HELMET configuration for Security Policy (CSP)
+const scriptSrcUrls = ['https://unpkg.com/', 'https://tile.openstreetmap.org'];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/'
+];
+const connectSrcUrls = ['https://unpkg.com', 'https://tile.openstreetmap.org'];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+ 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls]
+    }
+  })
+);
+// -------------------------------------------
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // Development Logging
 if (process.env.NODE_ENV === 'development') {
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,28 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations)
+
+// LEAFLET CONFIG
+var map = L.map('map', { zoomControl: false });
+ 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+ 
+const points = [];
+locations.forEach((loc) => {
+  points.push([loc.coordinates[1], loc.coordinates[0]]);
+  L.marker([loc.coordinates[1], loc.coordinates[0]])
+    .addTo(map)
+    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, { autoClose: false })
+    .openPopup();
+});
+ 
+const bounds = L.latLngBounds(points).pad(.75);
+map.fitBounds(bounds);
+ 
+map.scrollWheelZoom.disable();
+
+// MAPBOX CONFIG
 // export const displayMap = locations => {
 //   mapboxgl.accessToken =
 //     'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A';
