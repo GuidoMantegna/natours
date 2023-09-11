82a14f9b23a21cc8240883b6b49292c453d4714a
Guido Mantegna <gmantegna@elcallao.com>
2023-09-11 18:23:36 -0300

    Add 'logout' feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,6 +93,15 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  // Instead of sending a token, we send 'loggedout'
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
+
 // Middleware function for protected routes
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
