9f4e60aa62ddf9f5d718c0505d8c3a519f466e72
Guido Mantegna <gmantegna@elcallao.com>
2023-09-13 07:23:40 -0300

    Build the User Account page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,12 +4,9 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-/* Putting this mid. here, before all the other routes, so then it will be put 
-in the middleware stack for each and every request that comes in. */
-router.use(authController.isLoggedIn)
-
-router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', viewsController.getTour);
-router.get('/login', viewsController.getLoginForm);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 module.exports = router;
