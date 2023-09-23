fae02b61ab3fa5b7c625f252d0b648c59a1894d2
Guido Mantegna <gmantegna@elcallao.com>
2023-09-23 17:12:02 -0300

    complete the CRUD operations for our Booking API

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -8,17 +8,17 @@ router.use(authController.protect);
 // This route will only be for the client to get a checkout session.
 router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
-// router.use(authController.restrictTo('admin', 'lead-guide'));
+router.use(authController.restrictTo('admin', 'lead-guide'));
 
-// router
-//   .route('/')
-//   .get(bookingController.getAllBookings)
-//   .post(bookingController.createBooking);
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
 
-// router
-//   .route('/:id')
-//   .get(bookingController.getBooking)
-//   .patch(bookingController.updateBooking)
-//   .delete(bookingController.deleteBooking);
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
