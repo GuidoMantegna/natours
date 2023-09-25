89921a77e6821f8dc6541be0d54f03731e7675c7
Guido Mantegna <gmantegna@elcallao.com>
2023-09-25 07:28:14 -0300

    Modify .gitignore

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5819,7 +5819,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60107" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56690" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
