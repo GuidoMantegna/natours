794e5e6988e89ecb722301077bb098e9eb55a439
Guido Mantegna <gmantegna@elcallao.com>
2023-09-30 11:22:55 -0300

    Add 'showAlert()' to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import { login, logout } from './login';
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
